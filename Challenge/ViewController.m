79fd00c13cf7e162b4e72bc6a2b00d769097ada9
wanner <wanner@wanners-MacBook-Pro.local>
2013-09-18 16:02:47 +0200

    Umbau von Label auf Button für Zähler

--- Challenge/ViewController.m
+++ Challenge/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
+
+// ivar
+SystemSoundID mBeep;
 
 @interface ViewController ()
 
@@ -14,10 +18,47 @@
 
 @implementation ViewController
 
+@synthesize counter;
+
+-(IBAction)resetCounter:(id)sender
+{
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle:@"Rundenzähler zurücksetzen" message:@"Rundenzähler auf 0 zurücksetzen?" delegate:self cancelButtonTitle:@"Nein" otherButtonTitles:@"Ja", nil];
+    [alert show];
+}
+
+-(IBAction)incrementCounter:(id)sender
+{
+//    int c = counter.currentTitle.intValue+1;
+//    [counter setTitle: [NSString stringWithFormat:@"%d", c ] forState:UIControlStateNormal];
+    AudioServicesPlaySystemSound(mBeep);
+}
+
+-(void)alertView:(UIAlertView *) alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if(buttonIndex != [alertView cancelButtonIndex]) {
+        NSLog(@"Ok clicked");
+        [counter setTitle: @"0" forState:UIControlStateNormal];
+        AudioServicesPlaySystemSound(mBeep);
+    } else {
+        NSLog(@"Cancel clicked");
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // Create the sound ID
+    NSURL* beepURL = [[NSBundle mainBundle]
+                      URLForResource:@"Default" withExtension:@"wav"];
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef)beepURL, &mBeep);
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+	// Do any additional setup after loading the view, typically from a nib.
+    AudioServicesDisposeSystemSoundID(mBeep);
 }
 
 - (void)didReceiveMemoryWarning
