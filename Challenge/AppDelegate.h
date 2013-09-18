8ad6f178020df018fe05f4a8efa8e30fe0bd4770
wanner <wanner@wanners-MacBook-Pro.local>
2013-09-18 13:02:33 +0200

    Initial Commit

---
+++ Challenge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Challenge
+//
+//  Created by wanner on 18.09.13.
+//  Copyright (c) 2013 Gerhard Wanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Challenge/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Challenge
+//
+//  Created by wanner on 18.09.13.
+//  Copyright (c) 2013 Gerhard Wanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
