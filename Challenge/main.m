b51512718b765383daa6eff7d1a7801234558036
wanner <wanner@wanners-MacBook-Pro.local>
2013-09-18 13:02:33 +0200

    Initial Commit

---
+++ Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Challenge
+//
+//  Created by wanner on 18.09.13.
+//  Copyright (c) 2013 Gerhard Wanner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
